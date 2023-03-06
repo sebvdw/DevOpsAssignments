5dadd59160686b3997aaedda5e6d26e8cb89cade
sebvdw <sebastianvandw@gmail.com>
2023-03-06 12:57:09 +0100

    trash working

--- week2/assignment1-scripting/sum.sh
+++ week2/assignment1-scripting/sum.sh
@@ -3,10 +3,19 @@ if [ $# == 0 ]
 then
     echo "Please supply a data set"
 else
-    declare -i SUM=0
-    while read -r X ; do
-        SUM+=$X
-    done < "$1"
-    echo "Sum is: $SUM"
+# initialize a variable to store the sum
+sum=0
+
+# read each line of the file
+while read line; do
+  # extract the number from the line
+  number=$(echo "$line" | grep -Eo '[0-9]+')
+
+  # add the number to the sum
+  sum=$((sum + number))
+done < $1
+
+# output the sum
+echo "The sum of the numbers in the file is: $sum"
 fi
 
--- week2/assignment1-scripting/trash.sh
+++ week2/assignment1-scripting/trash.sh
@@ -5,6 +5,8 @@ if [ $# -eq 0 ]; then
   echo "    trash <file>     : Moves the file to the trash."
   echo "    trash -l         : Lists all the files in the trash."
   echo "    trash -r <file>  : Moves the file from the trash to the current directory."
+  echo "    trash -s <keyword> : Searches for files with the specified keyword in the filenames in the trash."
+  echo "    trash -sf <keyword> : Searches for files with the specified keyword in the filenames in the trash, including compressed archives."
   exit 1
 fi
 
@@ -17,6 +19,13 @@ elif [ "$1" = "-r" ]; then
   fi
   mv trash/$2 .
   echo "Moved $2 from trash to current directory."
+elif [ "$1" = "-s" ]; then
+  if [ $# -ne 2 ]; then
+    echo "Usage: trash -s <word>"
+    exit 1
+  fi
+  # search for word in files in trash and archive directory
+  grep -rHn "$2" trash/ trash/archive/
 else
   if [ ! -f "$1" ]; then
     echo "$1 is not a file."
@@ -27,4 +36,10 @@ else
   fi
   mv $1 trash/
   echo "Moved $1 to trash folder."
+  
+  # create archive directory if it does not exist
+  mkdir -p trash/archive
+
+  # create tarball from files in trash directory, excluding archive directory
+  tar -czvf trash/archive/$(date +"%Y-%m-%d-%H-%M").tar.gz --exclude='./archive' trash/*
 fi
